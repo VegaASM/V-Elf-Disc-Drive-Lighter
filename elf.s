26d081b3872617fc4e9dbbdd16d09d9e11fe5cf0
VegaASM <zackymcnell@yahoo.com>
2020-02-28 16:29:31 -0600

    Update elf.s

--- elf.s
+++ elf.s
@@ -1,5 +1,5 @@
 /*
-Copyright 2019 VegaASM
+Copyright 2020 VegaASM
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -14,15 +14,15 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-###########
-#START ELF#
-###########
+#~~~~~~~~~~~#
+# START ELF #
+#~~~~~~~~~~~#
 
 #
 
-############
-#ELF HEADER#
-############
+#~~~~~~~~~~~~#
+# ELF HEADER #
+#~~~~~~~~~~~~#
 
 ehdr:
 
@@ -48,57 +48,51 @@ ehdr:
 
     .long 0x00000001 #e_version, Elf Version 1 for Current. 0 = Invalid, 2+ = Version Num Increase. HBC requires this to always be 1
 
-    .long _start #e_entry, Entry Point Address, ALL PPC compilers will add 0x80000000 to this value.
+    .long 0x80004000 #e_entry, Entry Point Address, where assembler contents will be placed into Virtual Memory
     
     #Elf Contents for Flags, Program Headers, and Section Headers#
 
-    .long phdr_off #e_phoff #Address Offset to 1st Program Header
+    .long phdr_off #e_phoff #Offset from start of file to 1st Program Header
     .long 0x00000000 #e_shoff, Address Offset to 1st Section Header, leave Null since no Section Headers are used
     
-    .long 0x80000000 #e_flags, OS Specific Flags for the Elf Header, This flag is for PowerPC Embedded
+    .long 0x00000000 #e_flags, OS Specific Flags for the Elf Header, PowerPC has no flags, leave NULL
 
     .short ehdrsize #e_ehsize, size in bytes of ELF Header
     .short phdrsize #e_phentsize, size in bytes of Program Header
 
     .short 0x0001 #e_phnum, Amount of program headers
-    .short 0x0000 #e_shentsize, 1st Section Header size
+    .short 0x0000 #e_shentsize, 1st Section Header size, Null since no Section Headers are used
     
     .short 0x0000 #e_shnum, Amount of Section Headers
     .short 0x0000 #e_shstrndx # Address Offset to a specific Section Header within the Sectino Header Table
 
     ehdrsize = $ - ehdr #This macro calculates the size of the Elf Header in Bytes
 
-################
-#PROGRAM HEADER#
-################
+#~~~~~~~~~~~~~~~~#
+# PROGRAM HEADER #
+#~~~~~~~~~~~~~~~~#
 
-    phdr_off = phdr - 0x80000000 #This macro calculates the e_phoff value in the Elf Header
-    start_off = _start - 0x80000000 #This macro calculates the p_offset value in the Program Header
+    phdr_off = phdr - ehdr #This macro calculates the e_phoff value in the Elf Header
+    start_off = _start - ehdr #This macro calculates the p_offset value in the Program Header
 
 phdr:
 
     .long 0x00000001 #p_type aka p_load, The type of Program Loader. HBC requires this to always be 1
-    .long start_off #p_offset, Offset to next Program Header. Use entry point offset if only 1 Header is used.
-    .long _start #phdr #p_vaddr #Virtual Address to start of Program Header, value 0x80000000 auto added by all PPC compilers
-    .long _start #phdr #p_paddr #Physical Address to start of Program header, value 0x80000000 auto added by all PPC compilers
+    .long start_off #p_offset, Offset to next Program Header. Use offset from start of file to start of contents if only 1 Header is used.
+    .long 0x80004000 #phdr #p_vaddr #Virtual Address to start of Program Header, put Virtual entry point if only 1 Header is used
+    .long 0x80004000 #phdr #p_paddr #Physical Address to start of Program header, HBC wants this the same as Virtual. Put Virtual entry point if only 1 Header is used
 
     .long filesize #p_filesz, Program Header needs the File Size
     .long filesize #p_memsz, Program Header needs the Memory Size, for this source it's the same as the File Size
 
     .long 0x00000005 #p_flags, OS Specific Flags for the Program Header. 1 (Readable) + 4 (Executable) = 5
-    .long 0x00010000 #p_align, Align the Program Header, this indicates PowerPC -mrelocatable flag
+    .long 0x00000000 #p_align, Align the Program Header. No alignment needed for this source
 
     phdrsize = $ - phdr #This macro calculates the size of the Program Header in Bytes
 
-######################
-#PADDING FOR ELF LOAD#
-######################
-
-.zero 0x3FAC #The Wii + HBC requires the ELF to be loaded into Virtual Address 0x80004000
-
-###########
-#ASSEMBLER#
-###########
+#~~~~~~~~~~~~~~~~~~~~#
+# ASSEMBLER CONTENTS #
+#~~~~~~~~~~~~~~~~~~~~#
 
 _start:
     lis r3, 0xCD80
@@ -106,18 +100,14 @@ _start:
     stw r4, 0x00C0 (r3)
     b _start
     
-##########
-#FILESIZE#
-##########
+#~~~~~~~~~~~#
+# FILE SIZE #
+#~~~~~~~~~~~#
 
     filesize = $ - ehdr #This macro calculates the size of the entire ELF file in Bytes
 
 #
 
-#########
-#END ELF#
-#########
-
-
-
-
+#~~~~~~~~~#
+# END ELF #
+#~~~~~~~~~#
