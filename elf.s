28ba669bd161dd645f941719c24d61c223dbd4a7
VegaASM <zackymcnell@yahoo.com>
2021-04-04 07:54:43 -0500

    Update elf.s

--- elf.s
+++ elf.s
@@ -35,7 +35,7 @@ ehdr:
     .byte 0x01 #ei_class, 0 = Invalid, 1 = 32 bit, 2 = 64 bit
     .byte 0x02 #ei_data, Bit Encoding. 0 = Invalid, 1 = Little Endian (LSB), 2 = Big Endian (MSB)
     .byte 0x01 #ei_version, Read e_version below. HBC requires this to always be 1
-    .byte 0x00 #ei_osabi, OS or ABI specific Elf extensions. For the Wii (and required by HBC), this must always be 0
+    .byte 0x00 #ei_osabi, OS or ABI specific Elf extensions. HBC doesn't care about this byte
     
     #Elf Padding#
     
@@ -55,7 +55,7 @@ ehdr:
     .long phdr_off #e_phoff, Offset from start of ELF to 1st Program Header
     .long 0x00000000 #e_shoff, Address Offset to 1st Section Header, leave Null since no Section Headers are used
     
-    .long 0x00000000 #e_flags, OS Specific Flags for the Elf Header, PowerPC embedded flag is supposedly 0x80000000 but it's not needed for HBC, leave NULL
+    .long 0x80000000 #e_flags, OS Specific Flags for the Elf Header, PowerPC embedded flag is 0x80000000 but HBC doesn't care about this value
 
     .short ehdrsize #e_ehsize, size in bytes of ELF Header
     .short phdrsize #e_phentsize, size in bytes of Program Header
@@ -77,13 +77,13 @@ ehdr:
 
 phdr:
 
-    .long 0x00000001 #p_type aka p_load, The type of Program Loader. HBC requires this to always be 1
-    .long start_off #p_offset, Offset to next Program Header. However since there's only 1 program header, this must be set to the offset from start of ELF to where beginning of the assembler contents are located at
+    .long 0x00000001 #p_type aka p_load, The type of Program Loader. When this is set to 1, HBC then knows this is the program header that will have the p_offset value that points to entry point of assembly execution
+    .long start_off #p_offset, Offset to next Program Header. However since there's this the program header with p_load value of 1, this must be set to the offset from start of ELF to where beginning of the assembler contents are located at
     .long 0x80004000 #p_vaddr, Virtual Address to start of Program Header, put Virtual entry point if only 1 Header is used
     .long 0x00004000 #p_paddr, Physical Address to start of Program header, put Physical entry point if only 1 Header is used
 
     .long filesize #p_filesz, Program Header needs the File Size
-    .long filesize #p_memsz, Program Header needs the Memory Size, for this source it's the same as the File Size
+    .long filesize #p_memsz, Program Header needs the Memory Size
 
     .long 0x00000005 #p_flags, OS Specific Flags for the Program Header. 1 (Readable) + 4 (Executable) = 5
     .long 0x00000000 #p_align, Align the Program Header. No alignment needed for this source
@@ -97,8 +97,7 @@ phdr:
 _start:
     lis r3, 0xCD80
     li r4, 0x0020
-    stw r4, 0x00C0 (r3)
-    b _start
+    stw r4, 0x00C0 (r3) #Let an exception occur, just need the disc drive to be lit
     
 #~~~~~~~~~~~#
 # FILE SIZE #
